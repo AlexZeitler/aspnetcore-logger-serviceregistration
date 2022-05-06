9ee8dd659fecd8273c64ff8c2e9dda5ed25504e7
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-05-06 09:05:27 +0200

    chore: implement repro

---
+++ AspNetCoreLoggerInServiceRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreLoggerInServiceRegistration.Models;
+
+public class ErrorViewModel
+{
+  public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
